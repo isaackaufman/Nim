6a9c452f1edd15a37abeb01d11d7b64b2f6221e9
Isaac Kaufman <ikaufman588@gmail.com>
2015-02-12 02:02:25 -0500

    Revert "Add clone method & rework how pile is refilled"
    This reverts commit 747377933e14f18e7b96a42862db4faf73b31afd.

--- src/com/isaacdkaufman/nim/Pile.java
+++ src/com/isaacdkaufman/nim/Pile.java
@@ -12,26 +12,20 @@ public class Pile
 	// arrayList of Objects to represent the pile - as of now there is no reason for this not to be an int, but as an ArrayList, this allows for more scalability as far as versions of the package go
 	private final ArrayList<Object> gelt;
 
-	// amount of indicies in the ArrayList
-	private final int indicies;
-
 	// construct Pile object with given number of indicies in the ArrayList
 	public Pile (int numGelt)
 	{
 		// create the ArrayList
 		this.gelt = new ArrayList<Object>(numGelt);
 
-		// amount of indicies for use when repopulating the ArrayList
-		this.indicies = numGelt;
-
 		// populate the ArrayList
-		fillPile();
+		populateList(numGelt);
 	}
 
-	public void fillPile()
+	private void populateList(int numGelt)
 	{
 		// fill ArrayList with null Objects - TODO change this implementation maybe - Does it make sense to do it this way?
-		for (int i = 0; i < this.indicies; i++)
+		for (int i = 0; i < numGelt; i++)
 		{
 			this.gelt.add(new Object());
 		}
@@ -48,10 +42,5 @@ public class Pile
 		{
 			this.gelt.remove(this.gelt.size() - 1);
 		}
-	}
-
-	public Pile clone ()
-	{
-		return new Pile(this.indicies);
 	}
 }
