ca9ef2476f62a8e0bdfdefd9a0f736ba0f90b866
Isaac Kaufman <ikaufman588@gmail.com>
2015-02-13 01:06:36 -0500

    Find out how to work interfaces

--- src/com/isaacdkaufman/nim/Player.java
+++ src/com/isaacdkaufman/nim/Player.java
@@ -6,5 +6,12 @@ package com.isaacdkaufman.nim;
 
 public interface Player
 {
+
+	// Pile to act upon
+	//private Pile pile;
+
+	// max objects which can be taken
+	//private int maxObjects;
+
 	void takeTurn();
 }
