a3a22a2b44ebd9634099760dcfe474749c045e2c
Isaac Kaufman <ikaufman588@gmail.com>
2015-02-13 01:07:31 -0500

    AI for turn
    Looks clean, think about better implementation but I think this is it

--- src/com/isaacdkaufman/nim/SmartAIPlayer.java
+++ src/com/isaacdkaufman/nim/SmartAIPlayer.java
@@ -4,10 +4,41 @@
 **/
 package com.isaacdkaufman.nim;
 
-public class SmartAIPlayer
+import java.util.Random;
+
+public class SmartAIPlayer implements Player
 {
-	public void takeTurn()
+	// Pile to act upon
+	private Pile pile;
+
+	// max objects which can be taken
+	private int maxObjects;
+
+	// random number gen for when AI is second (can be in suboptimal positions)
+	private Random rand;
+
+	public SmartAIPlayer (Pile pile, int maxObjects)
 	{
-		
+		this.pile = pile;
+
+		this.maxObjects = maxObjects;
+
+		this.rand = new Random();
+	}
+
+	public void takeTurn ()
+	{
+		// if the number of objects in the Pile are not a multiple of the max objects which can be taken plus 1
+		if (!(this.pile.getSize() % (maxObjects + 1) == 0))
+		{
+			// take the objects which would leave the other player with a multiple of maxObjects + 1
+			this.pile.takeGelt(pile.getSize() % (maxObjects + 1));
+		}
+		else
+		{
+			// take a random number of objects within the constraints of the game - it does not matter what move is made here
+			// its best to try to throw the human player off as the game is unwinnable until he/she makes a mistake
+			this.pile.takeGelt(rand.nextInt(maxObjects) + 1);
+		}
 	}
 }
